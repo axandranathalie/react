de6849456e1f212ca52830de2eff56b7ad68e925
Alexandra <al3xandra92@hotmail.com>
2025-05-23 10:38:51 +0200

    created: new folder

---
+++ Lektion-3/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { Adder } from './components/Adder'
+import { List } from './components/List'
+import { PersonMe } from './components/PersonMe'
+import { PlusAdd } from './components/PlusAdd'
+
+
+function App() {
+  return (
+    <>
+    <Adder/>
+    <PlusAdd/>
+    <PersonMe/>
+    <List/>
+    </>
+  )
+}
+
+export default App
--- Skapa-komponent/src/App.tsx
+++ Skapa-komponent/src/App.tsx
@@ -1,17 +1,11 @@
 import './App.css'
-import { Adder } from './components/Adder'
-import { List } from './components/List'
-import { PersonMe } from './components/PersonMe'
-import { PlusAdd } from './components/PlusAdd'
+
 
 
 function App() {
   return (
     <>
-    <Adder/>
-    <PlusAdd/>
-    <PersonMe/>
-    <List/>
+
     </>
   )
 }
